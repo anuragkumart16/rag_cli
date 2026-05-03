b8fd90a3933531eb792c1738333d67a2c40a36f9
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2026-05-03 21:22:59 +0530

    added embeddings

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import FileReader from "./utils/readFile.utils.js"
 import createChunks from "./utils/chunks.utils.js"
+import generateEmbeddings from "./utils/embeddings.utils.js"
 
 const fileContents = FileReader.readTxtFile('notes.txt')
 
 const chunks  =  createChunks(fileContents)
 
-console.log(chunks)
+const embeddings = await generateEmbeddings(chunks)
+
+console.log(embeddings)
+
+
 
 
 
---
+++ src/utils/embeddings.utils.ts
@@ -0,0 +1,27 @@
+// This project uses ollama for generating embeddings
+// ideal code is 
+// brew install ollama
+// brew services start ollama
+// ollama pull nomic-embed-text
+// after these commands ollama starts running at http://localhost:11434 all we need is to make a post call 
+
+export async function generateOneEmbedding(text:string):Promise<number[]>{
+    const response = await fetch('http://localhost:11434/api/embeddings', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model: 'nomic-embed-text',
+            prompt: text
+        })
+    })
+
+    const data = await response.json()
+    return data.embedding
+}
+
+export default function generateEmbeddings(chunks:string[]):Promise<number[][]>{
+    const embeddingPromises = chunks.map(chunk => generateOneEmbedding(chunk))
+    return Promise.all(embeddingPromises)
+}
